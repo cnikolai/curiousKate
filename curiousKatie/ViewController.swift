3011f4babdbb7db9a2395ceaec5035997ea4ad89
Cynthia Nikolai <cynthianikolai@gmail.com>
2020-10-06 03:39:47 -0400

    Initial Commit

---
+++ curiousKatie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  curiousKatie
+//
+//  Created by Cynthia Nikolai on 10/6/20.
+//  Copyright © 2020 Cynthia Nikolai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
